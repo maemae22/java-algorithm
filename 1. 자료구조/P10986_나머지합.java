8a566a5d329c4cf9034954a8c8f9da60a776b0ad
maemae22 <maemae2@naver.com>
2023-02-10 18:55:47 +0900

    P10986_나머지합 / https://www.acmicpc.net/problem/10986

---
+++ 1. 자료구조/P10986_나머지합.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+// https://www.acmicpc.net/problem/10986
+public class P10986_나머지합 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        long[] sum = new long[N+1];
+        for (int i=1; i<=N; i++) {
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        long answer = 0;
+        long[] remain = new long[M];
+        for (int i=1; i<=N; i++) {
+            int num = (int) (sum[i]%M);
+            remain[num]++;
+            if (num == 0) {
+                answer++;
+            }
+        }
+
+        for (int i=0; i<M; i++) {
+            if (2 <= remain[i]) {
+                answer += remain[i]*(remain[i]-1)/2;
+            }
+        }
+        System.out.println(answer);
+    }
+}
