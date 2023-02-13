668b504419e4b45798429be597e9c82a2ed1b54b
maemae22 <maemae2@naver.com>
2023-02-13 22:08:58 +0900

    P12891_DNA비밀번호 / https://www.acmicpc.net/problem/12891

--- 1. 자료구조/P12891_DNA비밀번호.java
+++ 1. 자료구조/P12891_DNA비밀번호.java
@@ -3,91 +3,101 @@ import java.io.*;
 
 // https://www.acmicpc.net/problem/12891
 public class P12891_DNA비밀번호 {
-    static int checkArr[];
-    static int myArr[];
-    static int checkSecret;
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+    // 여러 메서드에서 동일하게 사용하고 싶을 경우, 전역변수(static)로 지정
+    static int[] nowCheck;
+    static int checkNum;
+    static int[] checkArr;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int S = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
-        int Result = 0;
-        char A[] = new char[S];
-        checkArr = new int[4];
-        myArr = new int[4];
-        checkSecret = 0;
-        A = bf.readLine().toCharArray();
-        st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < 4; i++) {
+
+        char[] dna = br.readLine().toCharArray();
+
+        nowCheck = new int[4]; // 현재 내꺼의 A C G T 개수
+        checkNum = 0; // A C G T 중 조건에 만족한 개수, 4가 될 경우 정답임
+        long answer = 0; // 정답 count
+
+        st = new StringTokenizer(br.readLine());
+        checkArr = new int[4]; // 문제에서 주어진 A C G T 최소 개수
+        for (int i=0; i<4; i++) {
             checkArr[i] = Integer.parseInt(st.nextToken());
-            if (checkArr[i] == 0)
-                checkSecret++;
+            if (checkArr[i] == 0) {
+                checkNum++;
+            }
         }
-        for (int i = 0; i < P; i++) { //초기 P부분 문자열 처리 부분
-            Add(A[i]);
+
+        // 초기 세팅
+        for (int i=0; i<P; i++) {
+            Add(dna[i]);
         }
-        if (checkSecret == 4)
-            Result++;
-        // 슬라이딩 윈도우 처리 부분
-        for (int i = P; i < S; i++) {
-            int j = i - P;
-            Add(A[i]);
-            Remove(A[j]);
-            if (checkSecret == 4)  // 4자리 수에 대한 크기가 모두 충족되었을 때는 유효한 비밀번호
-                Result++;
+        if (checkNum == 4) {
+            answer++;
         }
-        System.out.println(Result);
-        bf.close();
+
+        // 하나씩 옆으로 밀면서 비교 (슬라이딩 윈도우)
+        for (int i=P; i<S; i++) {
+            Remove(dna[i-P]);
+            Add(dna[i]);
+            if (checkNum == 4) {
+                answer++;
+            }
+        }
+
+        System.out.println(answer);
+
     }
 
-    private static void Add(char c) { //새로 들어온 문자를 처리해주는 함수
-        switch (c) {
-            case 'A':
-                myArr[0]++;
-                if (myArr[0] == checkArr[0])
-                    checkSecret++;
-                break;
-            case 'C':
-                myArr[1]++;
-                if (myArr[1] == checkArr[1])
-                    checkSecret++;
-                break;
-            case 'G':
-                myArr[2]++;
-                if (myArr[2] == checkArr[2])
-                    checkSecret++;
-                break;
-            case 'T':
-                myArr[3]++;
-                if (myArr[3] == checkArr[3])
-                    checkSecret++;
-                break;
+    private static void Add(char c) {
+        if (c == 'A') {
+            nowCheck[0]++;
+            if (nowCheck[0] == checkArr[0]) {
+                checkNum++;
+            }
+        } else if (c == 'C') {
+            nowCheck[1]++;
+            if (nowCheck[1] == checkArr[1]) {
+                checkNum++;
+            }
+        } else if (c == 'G') {
+            nowCheck[2]++;
+            if (nowCheck[2] == checkArr[2]) {
+                checkNum++;
+            }
+        } else if (c == 'T') {
+            nowCheck[3]++;
+            if (nowCheck[3] == checkArr[3]) {
+                checkNum++;
+            }
         }
     }
 
-    private static void Remove(char c) { //제거되는  문자를 처리해주는 함수
-        switch (c) {
-            case 'A':
-                if (myArr[0] == checkArr[0])
-                    checkSecret--;
-                myArr[0]--;
-                break;
-            case 'C':
-                if (myArr[1] == checkArr[1])
-                    checkSecret--;
-                myArr[1]--;
-                break;
-            case 'G':
-                if (myArr[2] == checkArr[2])
-                    checkSecret--;
-                myArr[2]--;
-                break;
-            case 'T':
-                if (myArr[3] == checkArr[3])
-                    checkSecret--;
-                myArr[3]--;
-                break;
+    private static void Remove(char c) {
+        if (c == 'A') {
+            if (nowCheck[0] == checkArr[0]) {
+                checkNum--;
+            }
+            nowCheck[0]--;
+        } else if (c == 'C') {
+            if (nowCheck[1] == checkArr[1]) {
+                checkNum--;
+            }
+            nowCheck[1]--;
+        } else if (c == 'G') {
+            if (nowCheck[2] == checkArr[2]) {
+                checkNum--;
+            }
+            nowCheck[2]--;
+        } else if (c == 'T') {
+            if (nowCheck[3] == checkArr[3]) {
+                checkNum--;
+            }
+            nowCheck[3]--;
         }
     }
+
 }
